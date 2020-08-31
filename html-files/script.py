5bdb729cee81c376679d44bca21b714479e2c616
Arnav <arnav77mathur@gmail.com>
2020-08-31 13:16:05 +0530

    deleted test files

--- html-files/script.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
-from pprint import pprint
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient(https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb)
-db=client.admin
-# Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
